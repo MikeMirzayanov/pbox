4506acab2f5ee70aa9577d7f51d1297a4e9caa1c
MikeMirzayanov <mirzayanovmr@gmail.com>
2021-09-06 16:02:42 +0300

    pbox-package.bat

--- scripts/pbox-package.bat
+++ scripts/pbox-package.bat
@@ -1,5 +1,5 @@
 if "%2"=="" (
-    pbox package %1 --templateDir=N:\pbox\templates --packageDir=N:\pbox\repo.pbox.me\1.0
+    pbox package %1 --templateDir=N:\pbox\templates.pbox.me --packageDir=N:\pbox\repo.pbox.me\1.0
 ) else (
-    pbox package %1 --templateDir=N:\pbox\templates --packageDir=N:\pbox\repo.pbox.me\1.0 --version=%2
+    pbox package %1 --templateDir=N:\pbox\templates.pbox.me --packageDir=N:\pbox\repo.pbox.me\1.0 --version=%2
 )
